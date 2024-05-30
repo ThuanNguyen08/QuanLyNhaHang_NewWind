015aa38611d02a9bddbe8e863f25a812dfdcb6cc
ThuanNguyen08 <cntt44dnguyenvanthuan0990@gmail.com>
2024-05-30 14:30:24 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { TrangchuComponent } from './component/trangchu/trangchu.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TrangchuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
